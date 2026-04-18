19e3775e86365fc76b89ca70436708d49bbd2745
Lumyra <sparktitan99@gmail.com>
2026-04-19 00:21:06 +0530

    Change GEMINI_API_KEY to a placeholder
    Updated GEMINI_API_KEY to use a placeholder instead of a hardcoded value.

--- gemini_guider.py
+++ gemini_guider.py
@@ -1,7 +1,7 @@
 import os 
 from google import genai
 
-GEMINI_API_KEY="AIzaSyB4D4z_NZHYE3bajnDCwOYtAvN5MrvLhq0"
+GEMINI_API_KEY="api_key"
 client=genai.Client(api_key=GEMINI_API_KEY)
 
 MODEL_NAME="gemini-flash-lite-latest"
